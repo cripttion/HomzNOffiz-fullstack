11f79f898bdee1a737c7c64e7af99a552b4fb68f
cripttion <pulakshri@gmail.com>
2023-04-24 01:49:54 +0530

    first commit

---
+++ my-app/src/Products.js
@@ -0,0 +1,112 @@
+const products = [
+    {
+        id:1,
+        image:"https://images.unsplash.com/photo-1597045566677-8cf032ed6634?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        name:"Nike air jordan",
+        category:"nike",
+        description:"wonderful shoes",
+        price: 10000
+
+    },
+    {
+        id:2,
+        image:"https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        name:"Adidas white sneaker",
+        category:"Adidas",
+        description:"Long lasteing shoes",
+        price: 15000
+
+    },
+    {
+        id:3,
+        image:"https://images.unsplash.com/photo-1609250291996-fdebe6020a8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        name:"Puma sports",
+        category:"Puma",
+        description:"Adidas the ronaldos fav.",
+        price: 5000
+
+    },
+    {
+        id:5,
+        image:"https://images.unsplash.com/photo-1580977252013-f5d20c3303ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80",
+        name:"Rebok color",
+        category:"Rebook",
+        description:"Home use slipper",
+        price: 30000
+
+    },
+    {
+        id:6,
+        image:"https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+        name:"uspolo balck",
+        category:"usPolo",
+        description:"Home use slipper",
+        price: 2000
+
+    },
+    {
+        id:7,
+        image:"https://images.unsplash.com/photo-1529810313688-44ea1c2d81d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1882&q=80",
+        name:"campus green",
+        category:"Campus",
+        description:"Home use slipper",
+        price: 1000
+
+    },
+
+    {
+        id:8,
+        image:"https://images.unsplash.com/photo-1597045566677-8cf032ed6634?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        name:"Nike air jordan",
+        category:"nike",
+        description:"wonderful shoes",
+        price: 10000
+
+    },
+    {
+        id:9,
+        image:"https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        name:"Adidas white sneaker",
+        category:"Adidas",
+        description:"Long lasteing shoes",
+        price: 15000
+
+    },
+    {
+        id:10,
+        image:"https://images.unsplash.com/photo-1609250291996-fdebe6020a8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        name:"Puma sports",
+        category:"Puma",
+        description:"Adidas the ronaldos fav.",
+        price: 5000
+
+    },
+    {
+        id:11,
+        image:"https://images.unsplash.com/photo-1580977252013-f5d20c3303ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80",
+        name:"Rebok color",
+        category:"Rebook",
+        description:"Home use slipper",
+        price: 30000
+
+    },
+    {
+        id:12,
+        image:"https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+        name:"uspolo balck",
+        category:"usPolo",
+        description:"Home use slipper",
+        price: 2000
+
+    },
+    {
+        id:13,
+        image:"https://images.unsplash.com/photo-1529810313688-44ea1c2d81d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1882&q=80",
+        name:"campus green",
+        category:"Campus",
+        description:"Home use slipper",
+        price: 1000
+
+    },
+]
+export default products;
